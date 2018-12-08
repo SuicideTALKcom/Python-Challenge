4ce49df95a7e87f40e899af2ba98476fe378d31d
DFN <rice@dfnworld.com>
2018-12-08 09:06:21 -0600

    original upload of work in progress

---
+++ PyBank/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,60 @@
+import os
+import csv
+
+month_count = 0
+date_list = []
+profit_loss_list = []
+total_profit_loss = float(0)
+change_value_list = []
+prior_value = float(0)
+
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+with open(csvpath, 'r', newline='') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csv_reader)
+
+    for value in csv_reader:
+        month_count = month_count + 1
+        date_list.append(str(value[0]))
+        profit_loss_list.append(float(value[1]))
+
+        current_value = value[1]
+        change_value = float(current_value) - float(prior_value)
+        change_value_list.append(change_value)
+        prior_value = current_value
+        
+def average(change_value_list):
+    x = len(change_value_list)
+    total = sum(change_value_list) - change_value_list[0]
+    avg = total / (x - 1)
+    return avg
+
+average_change = round(average(change_value_list), 2)
+
+total_profit_loss = round(sum(profit_loss_list))
+highest_profit_loss = round(max(profit_loss_list))
+lowest_profit_loss = round(min(profit_loss_list))
+highest_index = profit_loss_list.index(highest_profit_loss)
+lowest_index = profit_loss_list.index(lowest_profit_loss)
+
+print("Financial Analysis")
+print("-------------------------------------------------")
+print(f"Total Months: {month_count}")
+print(f"Total: ${total_profit_loss}")
+print(f"Average Change: ${average_change}")
+print(f"Greatest Increase in Profits: {date_list[highest_index]} (${highest_profit_loss})")
+print(f"Greatest Decrease in Profits: {date_list[lowest_index]} (${lowest_profit_loss})")
+
+output_path = os.path.join("financial_analysis.txt")
+with open(output_path, "w", newline="") as text_file:
+
+    print("Financial Analysis", file=text_file)
+    print("-------------------------------------------------", file=text_file)
+    print(f"Total Months: {month_count}", file=text_file)
+    print(f"Total: ${total_profit_loss}", file=text_file)
+    print(f"Average Change: ${average_change}", file=text_file)
+    print(f"Greatest Increase in Profits: {date_list[highest_index]} (${highest_profit_loss})", file=text_file)
+    print(f"Greatest Decrease in Profits: {date_list[lowest_index]} (${lowest_profit_loss})", file=text_file)
+
+    csvfile.close()
